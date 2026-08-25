d689c993ab16243c2d3441beddaf314906729411
ziqiancode <ziqiangao0@gmail.com>
2026-08-24 23:07:46 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // 2) Copy Project URL + anon/public key here.
 // IMPORTANT: NEVER put your Supabase service_role key in this file.
 window.SCHOOOL_CONFIG = {
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE"
+  SUPABASE_URL: "https://bbgmoiaqepfstyexicyi.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_WoqUHuqJvQriBgOBU-4M5Q_RoJVriz5"
 };
